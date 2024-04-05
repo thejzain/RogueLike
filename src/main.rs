12b1aeaa138b2c6a33e74847416e6892f92adf80
thejzain <thejzainkp@gmail.com>
2024-04-05 23:17:41 +0530

    0

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use tcod::colors::*;
+use tcod::console::*;
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const LIMIT_FPS: i32 = 20;
+
+struct Tcod {
+    root: Root,
+}
+fn main() {
+    let root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("RogueLike")
+        .init();
+
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let mut tcod = Tcod { root };
+
+    while !tcod.root.window_closed() {
+        tcod.root.set_default_foreground(WHITE);
+        tcod.root.clear();
+        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root.flush();
+        tcod.root.wait_for_keypress(true);
+    }
+}
