7eded048f5487138db51606e5a573177c7267ff9
thejzain <thejzainkp@gmail.com>
2024-04-06 12:40:20 +0530

    rooms and tunnel

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+use std::cmp;
+
 use tcod::colors::*;
 use tcod::console::*;
-use tcod::input::Key;
-use tcod::input::KeyCode::*;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
-const LIMIT_FPS: i32 = 45;
 
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
+
+const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
 
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
 const COLOR_DARK_GROUND: Color = Color {
@@ -17,17 +18,15 @@ const COLOR_DARK_GROUND: Color = Color {
     b: 150,
 };
 
+struct Tcod {
+    root: Root,
+    con: Offscreen,
+}
+
 type Map = Vec<Vec<Tile>>;
 
 struct Game {
     map: Map,
-}
-
-fn make_map() -> Map {
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
-    map
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -35,6 +34,7 @@ struct Tile {
     blocked: bool,
     block_sight: bool,
 }
+
 impl Tile {
     pub fn empty() -> Self {
         Tile {
@@ -42,6 +42,7 @@ impl Tile {
             block_sight: false,
         }
     }
+
     pub fn wall() -> Self {
         Tile {
             blocked: true,
@@ -50,12 +51,33 @@ impl Tile {
     }
 }
 
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Object {
     x: i32,
     y: i32,
     char: char,
     color: Color,
 }
+
 impl Object {
     pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
         Object { x, y, char, color }
@@ -74,30 +96,36 @@ impl Object {
     }
 }
 
-struct Tcod {
-    root: Root,
-    con: Offscreen,
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
 }
 
-fn handle_keys(tcod: &mut Tcod, player: &mut Object, game: &Game) -> bool {
-    let key = tcod.root.wait_for_keypress(true);
-    match key {
-        Key { code: Up, .. } => player.move_by(0, -1, game),
-        Key { code: Down, .. } => player.move_by(0, 1, game),
-        Key { code: Left, .. } => player.move_by(-1, 0, game),
-        Key { code: Right, .. } => player.move_by(1, 0, game),
-        Key {
-            code: Enter,
-            left_alt: true,
-            ..
-        } => {
-            let fullscreen = tcod.root.is_fullscreen();
-            tcod.root.set_fullscreen(!fullscreen);
-        }
-        Key { code: Escape, .. } => return true,
-        _ => {}
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
     }
-    false
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn make_map() -> Map {
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
+    create_h_tunnel(25, 55, 23, &mut map);
+
+    map
 }
 
 fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
@@ -113,9 +141,11 @@ fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
             }
         }
     }
+
     for object in objects {
         object.draw(&mut tcod.con);
     }
+
     blit(
         &tcod.con,
         (0, 0),
@@ -126,29 +156,65 @@ fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
         1.0,
     );
 }
-fn main() {
-    let root = Root::initializer()
-        .font("arial10x10.png", FontLayout::Tcod)
-        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
-        .title("RogueLike")
-        .init();
-    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
 
+fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        }
+        Key { code: Escape, .. } => return true, // exit game
+
+        Key { code: Up, .. } => player.move_by(0, -1, game),
+        Key { code: Down, .. } => player.move_by(0, 1, game),
+        Key { code: Left, .. } => player.move_by(-1, 0, game),
+        Key { code: Right, .. } => player.move_by(1, 0, game),
+
+        _ => {}
+    }
+
+    false
+}
+
+fn main() {
     tcod::system::set_fps(LIMIT_FPS);
 
+    let root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust/libtcod tutorial")
+        .init();
+
+    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
+
     let mut tcod = Tcod { root, con };
-    let game = Game { map: make_map() };
-    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
+
+    let player = Object::new(25, 23, '@', WHITE);
+
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
+
     let mut objects = [player, npc];
+
+    let game = Game { map: make_map() };
 
     while !tcod.root.window_closed() {
         tcod.con.clear();
 
         render_all(&mut tcod, &game, &objects);
+
         tcod.root.flush();
+
         let player = &mut objects[0];
-        let exit = handle_keys(&mut tcod, player, &game);
+        let exit = handle_keys(&mut tcod, &game, player);
         if exit {
             break;
         }
