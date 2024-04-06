508a714bbe810cdcf1a962c16736d2900fb52b31
thejzain <thejzainkp@gmail.com>
2024-04-06 12:58:56 +0530

    rect update

--- src/main.rs
+++ src/main.rs
@@ -68,6 +68,17 @@ impl Rect {
             y2: y + h,
         }
     }
+    pub fn center(&self) -> (i32, i32) {
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        (self.x1 <= other.x2)
+            && (self.x2 >= other.x1)
+            && (self.y1 <= other.y2)
+            && (self.y2 >= other.y1)
+    }
 }
 
 #[derive(Debug)]
