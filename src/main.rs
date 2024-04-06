7a34807f3386aedb767460ff9cb8f7c7bf3f1a9f
thejzain <thejzainkp@gmail.com>
2024-04-06 14:00:20 +0530

    map generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use std::cmp;
 
 use tcod::colors::*;
@@ -10,6 +11,10 @@ const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
 
 const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
+
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
 
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
 const COLOR_DARK_GROUND: Color = Color {
@@ -127,14 +132,56 @@ fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
     }
 }
 
-fn make_map() -> Map {
+fn make_map(player: &mut Object) -> Map {
     let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
 
-    let room1 = Rect::new(20, 15, 10, 15);
-    let room2 = Rect::new(50, 15, 10, 15);
-    create_room(room1, &mut map);
-    create_room(room2, &mut map);
-    create_h_tunnel(25, 55, 23, &mut map);
+    let mut rooms = vec![];
+    for _ in 0..MAX_ROOMS {
+        let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE..=ROOM_MAX_SIZE + 1);
+        let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE..=ROOM_MAX_SIZE + 1);
+
+        let x = rand::thread_rng().gen_range(0..=MAP_WIDTH - w);
+        let y = rand::thread_rng().gen_range(0..=MAP_HEIGHT - h);
+        let new_room = Rect::new(x, y, w, h);
+        let failed = rooms
+            .iter()
+            .any(|other_room| new_room.intersects_with(other_room));
+
+        if !failed {
+            create_room(new_room, &mut map);
+
+            let (new_x, new_y) = new_room.center();
+
+            if rooms.is_empty() {
+                player.x = new_x;
+                player.y = new_y;
+            }
+            if rooms.is_empty() {
+                // this is the first room, where the player starts at
+                // ...
+            } else {
+                // all rooms after the first:
+                // connect it to the previous room with a tunnel
+
+                // center coordinates of the previous room
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+                // toss a coin (random bool value -- either true or false)
+                if rand::random() {
+                    // first move horizontally, then vertically
+                    create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+                    create_v_tunnel(prev_y, new_y, new_x, &mut map);
+                } else {
+                    // first move vertically, then horizontally
+                    create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+                    create_h_tunnel(prev_x, new_x, new_y, &mut map);
+                }
+            }
+
+            // finally, append the new room to the list
+            rooms.push(new_room);
+        }
+    }
 
     map
 }
@@ -215,7 +262,9 @@ fn main() {
 
     let mut objects = [player, npc];
 
-    let game = Game { map: make_map() };
+    let game = Game {
+        map: make_map(&mut objects[0]),
+    };
 
     while !tcod.root.window_closed() {
         tcod.con.clear();
